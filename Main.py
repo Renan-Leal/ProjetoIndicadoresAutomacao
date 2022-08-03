0c27c38a883fcda8e5fbbc1dfdba48b5172a8d39
Renan Duarte Leal <renan.leallol@gmail.com>
2022-08-02 23:03:41 -0300

    deploy2

--- Main.py
+++ Main.py
@@ -3,9 +3,9 @@ from ManipularDados import ManipularDados, CalcularIndicadores
 from OrganizarPastas import OrganizarPastas
 
 #Importando planilhas
-emails_df = pd.read_excel(r'C:\Users\Renan\Desktop\PROJETO01\Bases de Dados\Emails.xlsx')
-lojas_df = pd.read_csv(r'C:\Users\Renan\Desktop\PROJETO01\Bases de Dados\Lojas.csv', sep=';', encoding='latin1')
-vendas_df = pd.read_excel(r'C:\Users\Renan\Desktop\PROJETO01\Bases de Dados\Vendas.xlsx')
+emails_df = pd.read_excel(r'C:\Users\Renan\Desktop\AutomacaoIndicadores\Bases de Dados\Emails.xlsx')
+lojas_df = pd.read_csv(r'C:\Users\Renan\Desktop\AutomacaoIndicadores\Bases de Dados\Lojas.csv', sep=';', encoding='latin1')
+vendas_df = pd.read_excel(r'C:\Users\Renan\Desktop\AutomacaoIndicadores\Bases de Dados\Vendas.xlsx')
 
 #Definindo dia do indicador a ser calculado e criando 1 planilha para cada loja
 dia_indicador = ManipularDados().definir_dia_indicador(vendas_df)
@@ -13,7 +13,7 @@ vendas_df = vendas_df.merge(lojas_df, on='ID Loja')
 dic_tabelas_lojas = ManipularDados().separar_por_loja(lojas_df, vendas_df)
 
 #Criando pastas das lojas e colocando uma planilha de loja em sua respectiva pasta
-caminho_backup = r'C:\Users\Renan\Desktop\PROJETO01\Backup Arquivos Lojas'
+caminho_backup = r'C:\Users\Renan\Desktop\AutomacaoIndicadores\Backup Arquivos Lojas'
 OrganizarPastas().organizar_pastas(caminho_backup, dia_indicador, dic_tabelas_lojas)
 
 #Calculando indicadores
